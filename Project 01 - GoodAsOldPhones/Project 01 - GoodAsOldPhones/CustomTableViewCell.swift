22e5b88ebf4df97d499b3d21bc27f49d6346249b
김준수 <rhdeotod1023@gmail.com>
2022-05-24 16:42:08 +0900

    UI생성_Project01

--- Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/CustomTableViewCell.swift
+++ Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/CustomTableViewCell.swift
@@ -2,35 +2,22 @@
 //  CustomTableViewCell.swift
 //  Project 01 - GoodAsOldPhones
 //
-//  Created by 준수김 on 2022/05/23.
+//  Created by 준수김 on 2022/05/24.
 //
 
 import UIKit
-import SnapKit
 
 class CustomTableViewCell: UITableViewCell {
-    
-    private let img: UIImageView = {
-        let imgView = UIImageView()
-        return imgView
-    }()
-    
-    private let label: UILabel  = {
-        let label = UILabel ()
-        return label
-    }()
-    
-    private func setConstraint() {
-        contentView.addSubview(img)
-        contentView.addSubview(label)
-        
-        img.snp.makeConstraints{ make in
-           
-        }
-        
-        label.snp.makeConstraints{ make in
-            
-        }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
     
 }
---
+++ Project 01 - GoodAsOldPhones/Project 01 - GoodAsOldPhones/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  Project 01 - GoodAsOldPhones
+//
+//  Created by 준수김 on 2022/05/24.
+//
+
+import Foundation
